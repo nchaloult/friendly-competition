1c08aa0146db257df1a5507d0bd2eb564af4e7a0
Nick Chaloult <npchaloult@gmail.com>
2019-05-18 16:24:44 -0400

    Making sure that I can send requests and receive responses from Riot with requests npm module

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const urls = require('./urls');
+
 const express = require('express');
+const request = require('request');
 
 const app = express();
 const PORT = process.env.port || 3001;
 
 app.get('/summoner', (req, res) => {
+  // Getting the summoner object from Riot
   const summonerUrl = urls.summoner + 'tonyl' + urls.api;
+  request(summonerUrl, { json: true }, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
 
-  res.send(String(summonerUrl));
+    console.log('====== Response ======');
+    console.log(res);
+    console.log();
+
+    console.log('====== Body ======');
+    console.log(body);
+    console.log();
+  });
+
+  res.send('Closed');
 });
 
 app.listen(PORT, () => {
