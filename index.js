78fa1193f2f7675bd465db1776d15059a2a42b07
Nick Chaloult <npchaloult@gmail.com>
2019-05-18 17:45:47 -0400

    Successfully made API call with promises and axios

--- index.js
+++ index.js
@@ -13,50 +13,18 @@ app.get('/summoner', (req, res) => {
   // Building API request URL to get summoner object
   const summonerUrl = urls.summoner + 'tonyl' + urls.api;
 
-//  request(summonerUrl, { json: true }, (err, res, body) => {
-//    if (err) {
-//      return console.log(err);
-//    }
-//
-//    console.log('====== Response ======');
-//    console.log(res);
-//    console.log();
-//
-//    console.log('====== Body ======');
-//    console.log(body);
-//    console.log();
-//  });
-
-//  axios.get(summonerUrl)
-//    .then(summonerObj => {
-//      // Build API request URL to get recent matches
-//      const matchListUrl = urls.matchList + summonerObj.accountId + urls.api + '&endIndex=' + NUM_MATCHES;
-//
-//      return axios.get(matchListUrl);
-//    })
-//    .then(matchListObj => {
-//      console.log('We got a response of a list of matches');
-//      res.send('Hooray');
-//    })
-//    .catch(err => {
-//      console.log(err);
-//      res.send('Error! Check console.');
-//    });
-
-  getSummonerObj(summonerUrl);
+  axios.get(summonerUrl)
+    .then(summonerObjRes => {
+      const summonerObj = summonerObjRes.data;
+      res.send(summonerObj.accountId);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send('Error! Check console.');
+    });
 });
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}....`);
 });
 
-const getSummonerObj = async url => {
-  try {
-    const response = await axios.get(url);
-    const data = response.data;
-
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-};
