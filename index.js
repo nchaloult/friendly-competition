f87fd9a60a581a744b03eba96218275533d98cb2
Nick Chaloult <npchaloult@gmail.com>
2019-05-18 18:02:28 -0400

    Chained API calls, fetching a match from an account ID

--- index.js
+++ index.js
@@ -4,19 +4,31 @@ const express = require('express');
 const axios = require('axios');
 
 // Number of previous matches to fetch stats from
-const NUM_MATCHES = 10;
+const NUM_MATCHES = 1;
 
 const app = express();
 const PORT = process.env.port || 3001;
 
 app.get('/summoner', (req, res) => {
+  let overallOutput = {
+    summonerName: null,
+    numMatches: NUM_MATCHES
+  }
+
   // Building API request URL to get summoner object
   const summonerUrl = urls.summoner + 'tonyl' + urls.api;
 
   axios.get(summonerUrl)
     .then(summonerObjRes => {
       const summonerObj = summonerObjRes.data;
-      res.send(summonerObj.accountId);
+      overallOutput.summonerName = summonerObj.name;
+
+      return axios.get(urls.matchList + summonerObj.accountId + urls.api + '&endIndex=' + NUM_MATCHES);
+    })
+    .then(matchListRes => {
+      const matchListObj = matchListRes.data;
+
+      res.json(matchListObj);
     })
     .catch(err => {
       console.log(err);
