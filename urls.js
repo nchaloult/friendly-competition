25fbe3b2c0d933593e719e8289957a34b4e9d03e
Nick Chaloult <npchaloult@gmail.com>
2019-05-18 16:06:14 -0400

    Moved URL strings (with API key) into separate file

---
+++ urls.js
@@ -0,0 +1,6 @@
+module.exports = {
+  summoner: 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/',
+  matchList: 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/',
+  match: 'https://na1.api.riotgames.com/lol/match/v4/matches/',
+  api: '?api_key=RGAPI-714c3239-7123-47f9-9f47-574b6f4b16f1',
+}
