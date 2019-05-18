dd59561ac74e8eb4294fc81e67999e9c88f89a4f
Nick Chaloult <npchaloult@gmail.com>
2019-05-18 16:31:04 -0400

    Migrated to axios for promise-based API calls

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const urls = require('./urls');
 
 const express = require('express');
-const request = require('request');
+const axios = require('axios');
 
 const app = express();
 const PORT = process.env.port || 3001;
 
 app.get('/summoner', (req, res) => {
-  // Getting the summoner object from Riot
+  // Building API request URL to get summoner object
   const summonerUrl = urls.summoner + 'tonyl' + urls.api;
-  request(summonerUrl, { json: true }, (err, res, body) => {
-    if (err) {
-      return console.log(err);
-    }
 
-    console.log('====== Response ======');
-    console.log(res);
-    console.log();
+//  request(summonerUrl, { json: true }, (err, res, body) => {
+//    if (err) {
+//      return console.log(err);
+//    }
+//
+//    console.log('====== Response ======');
+//    console.log(res);
+//    console.log();
+//
+//    console.log('====== Body ======');
+//    console.log(body);
+//    console.log();
+//  });
 
-    console.log('====== Body ======');
-    console.log(body);
-    console.log();
-  });
+  axios.get(summonerUrl)
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
   res.send('Closed');
 });
