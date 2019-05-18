03c4149cb2217014e93cc277689948d9f2e4a8b0
Nick Chaloult <npchaloult@gmail.com>
2019-05-18 16:09:05 -0400

    Updated API key

--- urls.js
+++ urls.js
@@ -2,5 +2,5 @@ module.exports = {
   summoner: 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/',
   matchList: 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/',
   match: 'https://na1.api.riotgames.com/lol/match/v4/matches/',
-  api: '?api_key=RGAPI-714c3239-7123-47f9-9f47-574b6f4b16f1',
+  api: '?api_key=RGAPI-7fcdb63b-3699-414d-be61-18fdb41b5396',
 }
