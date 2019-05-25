311c8ca916441ee78076fe62a3554097414a01fb
Nick Chaloult <npchaloult@gmail.com>
2019-05-24 21:00:54 -0400

    Ran into roadblock concerning having to wait for lots of promises to run to fetch data about recent matches

--- utils.js
+++ utils.js
@@ -27,6 +27,19 @@ const getRecentMatchStats = (accId, numMatches, potentialFriends, findFriends) =
       for (match of matchListObj.matches) {
         // TODO: remove - this is for testing
         output.gameIds.push(match.gameId);
+
+        // Building API request to get stats from a particular recent match
+        const matchReq = urls.match + match.gameId + urls.api;
+
+        /*
+         * TODO: Need some way to store all of these promises so that you can
+         * do Promise.all() on them or something. Otherwise, while data is
+         * being gathered from each of the recent matches, line 45 will resolve
+         * and return an incomplete output.
+         */
+        axios.get(matchReq)
+          .then(matchRes => {
+          });
       }
 
       resolve(output.gameIds);
