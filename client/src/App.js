78f52dc340b806b5bfec193158c71cd13caa34ae
Nick Chaloult <npchaloult@gmail.com>
2019-05-27 10:16:20 -0400

    Added footer markup

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,20 @@ import './index.css';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Friendly Competition</h1>
+    <div>
+
+      <div className="container">
+        <h1>Friendly Competition</h1>
+      </div>
+      <footer>
+        <div className="container">
+          <p><em>
+            Friendly Competition compares a League of Legends player's recent in-game performance with that of their friends.
+          </em></p>
+          <p>Built by <a href="https://github.com/nchaloult">Nick Chaloult</a> in 2019</p>
+        </div>
+      </footer>
+
     </div>
   );
 }
