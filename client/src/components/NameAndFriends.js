5220d3b191c24a7941e4b5af18158db77182d848
Nick Chaloult <npchaloult@gmail.com>
2019-06-09 15:50:45 -0400

    Put Highlighting Stats row in its own component

---
+++ client/src/components/NameAndFriends.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../resources/index.css';
+
+export default function NameAndFriends(props) {
+  return (
+    <div className="row">
+      <div className="col-4">
+        <h2>Highlighting Stats<br />For <span className="hilite">{ props.summName }</span></h2>
+      </div>
+      <div className="col-8">
+        <div className="row">
+          <div className="col-4" style={{"textAlign": "center"}}>
+            <h4>{ props.friend1SummName }</h4>
+            <p>played in<br />3 / 10 games</p>
+          </div>
+          <div className="col-4" style={{"textAlign": "center"}}>
+            <h4>Friend 1</h4>
+            <p>played in<br />3 / 10 games</p>
+          </div>
+          <div className="col-4" style={{"textAlign": "center"}}>
+            <h4>Friend 1</h4>
+            <p>played in<br />3 / 10 games</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/components/Summary.js
+++ client/src/components/Summary.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import '../resources/index.css';
 
+import NameAndFriends from './NameAndFriends';
+
 export default function Summary(props) {
   return (
     <div className="myCard">
-      <div className="row">
-        <div className="col-4">
-          <h2>Highlighting Stats<br />For <span className="hilite">{ props.summName }</span></h2>
-        </div>
-        <div className="col-8">
-          <div className="row">
-            <div className="col-4" style={{"textAlign": "center"}}>
-              <h4>{ props.friend1SummName }</h4>
-              <p>played in<br />3 / 10 games</p>
-            </div>
-            <div className="col-4" style={{"textAlign": "center"}}>
-              <h4>Friend 1</h4>
-              <p>played in<br />3 / 10 games</p>
-            </div>
-            <div className="col-4" style={{"textAlign": "center"}}>
-              <h4>Friend 1</h4>
-              <p>played in<br />3 / 10 games</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <NameAndFriends
+        summName={ props.summName }
+        friend1SummName={ props.friend1SummName }
+      />
     </div>
   );
 }
