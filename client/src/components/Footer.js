1d9e0895013fca4944d04b4c7b1b476305df8688
Nick Chaloult <npchaloult@gmail.com>
2019-06-09 17:54:16 -0400

    Reorganized footer contents

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,12 +5,18 @@ export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <p><em>
-          Built by <a href="https://github.com/nchaloult">Nick Chaloult</a> in 2019
-        </em></p>
-        <p><em>
-          Loading animation made by <a href="https://loading.io">loading.io</a>
-        </em></p>
+        <div className="row">
+          <div className="col" style={{ "textAlign": "right", "borderRight": "2px solid #414959" }}>
+            <p><em>
+              Built by <a href="https://github.com/nchaloult">Nick Chaloult</a> in 2019
+            </em></p>
+          </div>
+          <div className="col" style={{ "textAlign": "left" }}>
+            <p><em>
+              Loading animation made by <a href="https://loading.io">loading.io</a>
+            </em></p>
+          </div>
+        </div>
       </div>
     </footer>
   );
