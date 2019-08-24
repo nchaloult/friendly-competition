7c2c07e93cee9eab6ea78ec38ceae367321aba28
Nick Chaloult <npchaloult@gmail.com>
2019-08-23 23:38:03 -0400

    Added horizontal line separating friends played with from highlighting stats

--- client/src/components/NameAndFriends.js
+++ client/src/components/NameAndFriends.js
@@ -3,30 +3,33 @@ import '../resources/index.css';
 
 export default function NameAndFriends(props) {
   return (
-    <div className="row">
-      <div className="col-4">
-        <h2>
-          <span className="hilite">{ props.summName }</span>'s
-          <br />
-          Highlighting Stats
-        </h2>
-      </div>
-      <div className="col-8">
-        <div className="row">
-          <div className="col-4" style={{"textAlign": "center"}}>
-            <h4>{ props.friend1SummName }</h4>
-            <p>played in<br />{ props.friend1Count } / 10 games</p>
-          </div>
-          <div className="col-4" style={{"textAlign": "center"}}>
-            <h4>{ props.friend2SummName }</h4>
-            <p>played in<br />{ props.friend2Count } / 10 games</p>
-          </div>
-          <div className="col-4" style={{"textAlign": "center"}}>
-            <h4>{ props.friend3SummName }</h4>
-            <p>played in<br />{ props.friend3Count } / 10 games</p>
+    <div>
+      <div className="row">
+        <div className="col-4">
+          <h2>
+            <span className="hilite">{ props.summName }</span>'s
+            <br />
+            Highlighting Stats
+          </h2>
+        </div>
+        <div className="col-8">
+          <div className="row">
+            <div className="col-4" style={{"textAlign": "center"}}>
+              <h4>{ props.friend1SummName }</h4>
+              <p>played in<br />{ props.friend1Count } / 10 games</p>
+            </div>
+            <div className="col-4" style={{"textAlign": "center"}}>
+              <h4>{ props.friend2SummName }</h4>
+              <p>played in<br />{ props.friend2Count } / 10 games</p>
+            </div>
+            <div className="col-4" style={{"textAlign": "center"}}>
+              <h4>{ props.friend3SummName }</h4>
+              <p>played in<br />{ props.friend3Count } / 10 games</p>
+            </div>
           </div>
         </div>
       </div>
+      <hr />
     </div>
   );
 }
